26c5a9e324b3855d7585c5aaac8e33b70fd819be
Ydiren <andymg@gmail.com>
2023-12-13 15:40:38 +0000

    Added GetAllTemplates endpoint Added test

--- TemplatesApi.Tests/DatabaseTest.cs
+++ TemplatesApi.Tests/DatabaseTest.cs
@@ -53,4 +53,31 @@ public class DatabaseTest : TestClass<DatabaseFixture>
         getResult.Name.Should().Be("Test");
         getResult.CreatedAt.Should().NotBe(DateTimeOffset.UnixEpoch);
     }
+    
+    [Fact]
+    public async Task TemplateIsRetrievedWithAll()
+    {
+        var templateCount = 5;
+        await AddTemplates(templateCount);
+        
+        var (getResponse, getResult) = await _fixture.Client.GETAsync<GetAllTemplatesEndpoint, GetAllTemplatesResponse>();
+        getResponse.IsSuccessStatusCode.Should().BeTrue();
+        getResult.Templates.Should().HaveCount(templateCount);
+        getResult.Templates.First().Id.Should().NotBeEmpty();
+        getResult.Templates.First().Name.Should().NotBeNullOrWhiteSpace();
+        getResult.Templates.First().CreatedAt.Should().NotBeOnOrBefore(DateTimeOffset.UnixEpoch);
+    }
+
+    private async Task AddTemplates(int templateCount)
+    {
+        for (var i = 0; i < templateCount; i++)
+        {
+            var (response, result) = await _fixture.Client.POSTAsync<CreateTemplateEndpoint, CreateTemplateRequest, CreateTemplateResponse>(
+                new CreateTemplateRequest
+                {
+                    Name = _fixture.Fake.Name.FullName()
+                });
+            response.IsSuccessStatusCode.Should().BeTrue();
+        }
+    }
 }
---
+++ TemplatesApi/Templates/Retrieval/GetTemplates.cs
@@ -0,0 +1,56 @@
+﻿using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using TemplatesApi.Data;
+using TemplatesApi.Data.Entities;
+
+namespace TemplatesApi.Templates.Retrieval;
+
+sealed class GetAllTemplatesEndpoint : EndpointWithoutRequest<GetAllTemplatesResponse, GetAllTemplatesMapper>
+{
+    private readonly TemplatesDbContext _dbContext;
+
+    public GetAllTemplatesEndpoint(TemplatesDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public override void Configure()
+    {
+        Get("/api/templates");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken c)
+    {
+        var entities = await _dbContext.Templates.ToListAsync(c);
+        var response = new GetAllTemplatesResponse()
+        {
+            Templates = entities.Select(Map.FromEntity).ToList()
+        };
+        await SendOkAsync(response, c);    
+    }
+}
+
+sealed class GetAllTemplatesResponse
+{
+    public List<TemplateSummary> Templates { get; set; } = default!;
+}
+
+sealed class TemplateSummary
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset? ModifiedAt { get; set; }
+}
+
+sealed class GetAllTemplatesMapper : ResponseMapper<TemplateSummary, Template>
+{
+    public override TemplateSummary FromEntity(Template e) => new()
+    {
+        Id = e.Id,
+        Name = e.Name,
+        CreatedAt = e.CreatedAt,
+        ModifiedAt = e.ModifiedAt
+    };
+}
